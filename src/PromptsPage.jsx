bdddb30479ee6733817246544b9627af9af5f863
matt <aldraddd01@gmail.com>
2024-12-23 04:03:37 +0700

    added the prompts page

---
+++ src/PromptsPage.jsx
@@ -0,0 +1,9 @@
+export default function PromptsPage() {
+    return (
+        <>
+            <section className="flex justify-center items-center">
+                <h1 className="tracking-widest text-[3rem] text-slate-300">work in progress</h1>
+            </section>
+        </>
+    )
+}
